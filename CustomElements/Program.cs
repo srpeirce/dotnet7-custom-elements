2b6bb8d5867e569a11e122b79e5c992b3010a6b0
Steve Peirce <=>
2022-07-13 21:38:47 +0100

    Dotnet 7 custom elements example

---
+++ CustomElements/Program.cs
@@ -0,0 +1,11 @@
+using CustomElements.Shared;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.RegisterCustomElement<Counter>("my-counter");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
---
+++ README.md
@@ -0,0 +1,5 @@
+# dotnet7-custom-elements
+
+Super simple custom elements
+
+[Docs](https://docs.microsoft.com/en-gb/aspnet/core/blazor/components/?view=aspnetcore-6.0#blazor-custom-elements)
